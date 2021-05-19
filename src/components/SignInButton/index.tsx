4a9192e6c5589ff89c23dc3a53e380fa474a6ce1
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-19 20:09:19 -0300

    Adicionando autenticação com o github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [ session ] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  function handleSignIn() {
+    signIn("github");
+  }
+
+  function handleLogout(){
+    signOut();
+  }
+
+  return session ? (
+    <button className={styles.signInButton} onClick={handleLogout}>
       <FaGithub color="#04d401" />
-      adrianhideki
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button className={styles.signInButton} onClick={handleSignIn}>
       <FaGithub color="#eba417" /> Sign in with GitHub
     </button>
   );
