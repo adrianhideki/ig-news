82e3cc6de2b00961396d70a4c626748ed488365b
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-19 21:42:27 -0300

    Ajustando query para verificar antes de inserir no FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,7 +6,6 @@ import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const [ session ] = useSession();
-  console.log(session);
 
   function handleSignIn() {
     signIn("github");
