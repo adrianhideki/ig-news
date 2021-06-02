0c5836adcf45a490bc1aba73348702cc742688a4
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-06-01 22:22:35 -0300

    Movendo arquivo home

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages";
+import Home from "../../pages";
 
 jest.mock("next/router");
 jest.mock("next-auth/client", () => {
